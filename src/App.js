4c4efb4aaa76006511d1d61b966ad802241d89b1
Rj <rikrdo.ortega@gmail.com>
2019-11-19 16:43:43 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,12 @@ import React, { Component } from 'react';
 import Timer from './Timer'
 
 class App extends Component {
-
-  //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
   }
-
-
-  //Your code here:
-
-
-
-
-
-
-
-
-
-  // No need to modify anything in render or the class methods below
-  // Unless, of course, you're curious about how it all works
+  componentDidMount() {
+    this.handleAddTimer()
+  }
   render() {
 
     return (
@@ -45,7 +32,7 @@ class App extends Component {
   // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+      timerIDs: [...prevState.timerIDs, Math.floor(Math.random() * 1000)]
     }))
   }
 
